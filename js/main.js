4fdd862a6505caecde7e604670f90c483047efda
Craft Yun <craftyun83@gmail.com>
2025-03-08 18:07:37 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,143 @@
+var startLevel = parseInt(document.querySelector("#startingLevel").value)
+var endLevel = parseInt(document.querySelector("#endingLevel").value)
+var attribute = document.querySelector("#attributeSelector").value
+var running = false
+var prices = [[]]
+
+function formatNumber(num) {
+    if (num >= 1_000_000_000) {
+        return `${(num / 1_000_000_000).toFixed(2)}b`;
+    } else if (num >= 1_000_000) {
+        return `${(num / 1_000_000).toFixed(2)}m`;
+    } else if (num >= 1_000) {
+        return `${(num / 1_000).toFixed(2)}k`;
+    } else {
+        return num.toString();
+    }
+}
+
+async function calculatePrices() {
+    prices = [[]]
+    for (var i = 0; i < 10; i++) {
+        let result = await (await fetch("https://sky.coflnet.com/api/auctions/tag/ATTRIBUTE_SHARD/active/bin?"+attribute+"="+(i+1))).text();
+        prices.push(JSON.parse(result))
+    }
+    prices[startLevel].push({
+        "startingBid": 0, 
+        "uuid": "starting",
+        "nbtData" : {
+            "data": {
+                "attributes": {
+                    [attribute]: startLevel
+                }
+            }
+        }
+    })
+    prices[startLevel].sort((a, b) => {return a.startingBid > b.startingBid})
+}
+
+function cost(l, stack=[]) {
+    let rl = stack.slice()
+    let shard;
+    if (l == 1) {
+        if (prices[1].length == 0) return []
+        shard = prices[1].shift()
+        rl.push(shard)
+        return rl
+    }
+    let t1 = cost(l-1, rl)
+    let t2 = cost(l-1, rl)
+    let compareStack = t1.concat(t2);
+    let ranOut = t1.length == 0 || t2.length == 0
+    let noCurrent = prices[l].length == 0
+
+    let sum = compareStack.reduce((acc, item) => acc + item.startingBid, 0)
+
+    if (ranOut && !noCurrent) { 
+        rl.push(prices[l].shift())
+        compareStack.forEach((i) => {
+            let tier = i["nbtData"]["data"]["attributes"][attribute]
+            prices[tier].push(i)
+            prices[tier].sort((a, b) => {return a.startingBid > b.startingBid})
+        })
+        return rl
+    } if (noCurrent && !ranOut) {
+        return compareStack
+    } if (noCurrent && ranOut) {
+        return []
+    } if (prices[l][0]["startingBid"] <= sum) {
+        rl.push(prices[l].shift())
+        compareStack.forEach((i) => {
+            let tier = i["nbtData"]["data"]["attributes"][attribute]
+            prices[tier].push(i)
+            prices[tier].sort((a, b) => {return a.startingBid > b.startingBid})
+        })
+        return rl
+    }
+    return compareStack
+}
+
+function copyAuctionId(string) {
+    console.log(string)
+    navigator.clipboard.writeText(string);
+  } 
+
+document.querySelector("#attributeSelector").addEventListener("change", (input) => {
+    attribute = input.target.value
+})
+
+document.querySelector("#startingLevel").addEventListener("input", (input) => {
+    startLevel = parseInt(input.target.value)
+    document.querySelector('#startingLevelDisplay').textContent =  'Starting Level: '+input.target.value
+})
+
+document.querySelector("#endingLevel").addEventListener("input", (input) => {
+    endLevel = parseInt(input.target.value)
+    document.querySelector('#endingLevelDisplay').textContent =  'Ending Level: '+input.target.value
+})
+
+document.querySelector("#calculateButton").onclick = async () => {
+    if (endLevel <= startLevel) {alert("Invalid ending level!"); return false;}
+    if (running == true) return false;
+    document.querySelector("#resultsContainer").innerHTML = ""
+    running = true
+    prices=  [[]];
+    await calculatePrices();
+    let result = cost(endLevel);
+    result.sort((a, b) => {return a.startingBid > b.startingBid})
+    if (result.length == 0) {
+        document.querySelector("#resultsContainer").innerHTML = "Could not find a way to reach desired level!"
+    } else {
+        let sum = result.reduce((acc, item) => acc + item.startingBid, 0)
+        let sumElement = document.createElement("h2")
+        sumElement.textContent = "Total Cost: "+formatNumber(sum)
+        sumElement.classList.add("costSum")
+        document.querySelector("#resultsContainer").appendChild(sumElement)
+        result.forEach((book) => {
+            let bookElement = document.createElement("h3")
+            let tier = book["nbtData"]["data"]["attributes"][attribute]
+            bookElement.setAttribute("copyString", "/viewauction "+book.uuid)
+            if (book.startingBid == 0) return;
+            if (attribute == "mending") {
+                bookElement.textContent = "VITALITY "+tier+" @"+(formatNumber(book.startingBid))+": "+"/viewauction "+book.uuid
+            }else {
+                bookElement.textContent = attribute.toUpperCase()+" "+tier+" @"+(formatNumber(book.startingBid))+": "+"/viewauction "+book.uuid
+            }
+            let copybutton = document.createElement("button");
+            let copyicon = document.createElement('i');
+            copyicon.classList.add("ti")
+            copyicon.classList.add("ti-copy")
+
+            copybutton.addEventListener('click', (input) => {
+                let node = input.target.parentElement
+                if (node.tagName != "H3") node = node.parentElement;
+                copyAuctionId(node.getAttribute("copyString"))
+            })
+            copybutton.appendChild(copyicon)
+            bookElement.appendChild(copybutton)
+            document.querySelector("#resultsContainer").appendChild(bookElement)
+            document.querySelector("#resultsContainer").appendChild(document.createElement("br"))
+        })
+    }
+    running = false
+}
---
+++ main.py
@@ -0,0 +1,76 @@
+import requests
+
+startLevel = 1
+endLevel = 3
+attribute = "mending"
+
+prices = [[]]
+
+for i in range(10):
+    url = f"https://sky.coflnet.com/api/auctions/tag/ATTRIBUTE_SHARD/active/bin?{attribute}={str(i+1)}"
+    response = requests.request("GET", url)
+    prices.append(response.json())
+
+prices[startLevel].append({
+    "startingBid": 0, 
+    "uuid": "starting",
+    "nbtData" : {
+        "data": {
+            "attributes": {
+                attribute: startLevel
+            }
+        }
+    }
+})
+
+print(prices)
+
+prices[startLevel] = sorted(prices[startLevel], key=lambda x: x["startingBid"])
+
+def cost(l, stack=[]):
+    global prices
+    rl = stack.copy()
+    if l == 1:
+        if prices[1] == []:
+            return []
+        shard = prices[1].pop(0)
+        rl.append(shard)
+        return rl
+    
+    t1 = cost(l-1, rl)
+    t2 = cost(l-1, rl)
+    compareStack = t1+t2
+    ranOut = t1 == [] or t2 == []
+    noCurrent = prices[l] == []
+    if ranOut and not noCurrent:
+        rl.append(prices[l].pop(0))
+        for i in compareStack:
+            tier = i["nbtData"]["data"]["attributes"][attribute]
+            prices[tier].append(i)
+            prices[tier] = sorted(prices[tier], key=lambda x: x["startingBid"])
+        return rl
+    if noCurrent and not ranOut:
+        return compareStack
+    if noCurrent and ranOut:
+        return []
+    if prices[l][0]["startingBid"] <= sum(item["startingBid"] for item in (compareStack)):
+        rl.append(prices[l].pop(0))
+        for i in compareStack:
+            tier = i["nbtData"]["data"]["attributes"][attribute]
+            prices[tier].append(i)
+            prices[tier] = sorted(prices[tier], key=lambda x: x["startingBid"])
+        return rl
+    else:
+        return compareStack
+
+def filterOutBaseAttr(x):
+    return x["uuid"] != "starting"
+
+result = cost(endLevel)
+result = list(filter(filterOutBaseAttr, result))
+finalCost = sum(item["startingBid"] for item in (result))
+for auction in result:
+    tier = auction["nbtData"]["data"]["attributes"][attribute]
+    price = auction["startingBid"]
+    print(f"{attribute.upper()} {str(tier)} {str(price/1000000)}M: /viewauction {auction['uuid']}")
+print(f"TOTAL COST: {str(finalCost/1000000)}M")
